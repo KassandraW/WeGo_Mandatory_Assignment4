baa495c56f222d6013a820b31762a1922f1c14cc
kassandra <kwad@itu.dk>
2025-11-06 13:46:20 +0100

    state and loopoflife

--- src/node/node.go
+++ src/node/node.go
@@ -2,29 +2,56 @@ package main
 
 import (
 	proto "Mandatory4/grpc"
+	"context"
+	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 )
 
+// change states
+type NodeState int
+const (
+	StateReleased NodeState = iota
+	StateHeld
+	StateWanted
+)
+
 type Node struct { 
 	proto.UnimplementedNodeServer
 	lamport_clock 		int64
 	cs_access			bool
+	state				NodeState 		
+
 
 }
 
+
 func critical_section(){
-	log.Printf("Doing something in the critical section høhø")
+	fmt.Println("Doing something in the critical section høhø")
 }	
 
 
-func main() { //initializes server
+func loopOfLife(){
+	for{
+		// do some stuff that calculates if we want to access CS or not. 
+	}
+}
+
+
+func main() { 
 	server := &Node{}
+	server.state = StateReleased
 	server.lamport_clock = 0;
 	server.cs_access = false;
-	server.start_server() //starts the gRPC server
+	go loopOfLife();
+	server.start_server()
+}
+
+
+func (s *Node) Request(ctx context.Context, timestamp *proto.TimeStamp ) (*proto.Empty, error) {
+	return &proto.Empty{}, nil
 }
 
 func (s *Node) start_server() {
