8f36f8809494a44897dad0b5f01f15bdbd2eb420
Carla <cmoo@itu.dk>
2025-11-10 09:07:21 +0100

    Now it works!!  aparrently

--- src/node/node.go
+++ src/node/node.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"os"
 	"strings"
+
 	//"time"
 
 	"google.golang.org/grpc"
@@ -32,19 +33,17 @@ type Node struct {
 	node_connections []proto.NodeClient
 }
 
-
 func critical_section() {
 	fmt.Println("Doing something in the critical section høhø")
 }
 
 func loopOfLife() {
 	// do some stuff that calculates if we want to access CS or not.
-	for { 
-		//time.Sleep(time.Millisecond * 500) 
+	for {
+		//time.Sleep(time.Millisecond * 500)
 	}
-	
-}
 
+}
 
 func main() {
 	// set up log info
@@ -54,46 +53,46 @@ func main() {
 		log.Fatalf("could not open log file client: %v", err)
 	}
 	if err := os.Truncate(filepath, 0); // clear the log file on each run
-		err != nil {
+	err != nil {
 		log.Printf("Failed to truncate: %v", err)
 	}
 
 	defer Log_File.Close()
 
 	log.SetOutput(Log_File)
-    log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 
-	 //get server port from user
+	//get server port from user
 	reader := bufio.NewReader(os.Stdin)
-    fmt.Println("to add a Node pleas enter adress info of 1 node from Adress_file")
+	fmt.Println("to add a Node pleas enter adress info of 1 node from Adress_file")
 	log.Println("to add a Node pleas enter adress info of 1 node from Adress_file")
 	adressLine, err := reader.ReadString('\n')
 	adressLine = strings.TrimSuffix(adressLine, "\n")
-	fmt.Println("this is the adress line :" + adressLine)
+	//fmt.Println("this is the adress line :" + adressLine)
 
 	// Set server up
 	server := &Node{}
 	server.state = StateReleased
 	server.lamport_clock = 0
 	server.cs_access = false
-	
+
 	if err != nil {
 		fmt.Print("read input not avalable")
 		log.Fatalf("scanner failed")
 	}
 	// repeat for every node in a loop, adding them to the node_connections list
-	for i := 0; i < 3; i++ {fmt.Print("loooping!")}
-	conn, err := grpc.NewClient("localhost:5052", grpc.WithTransportCredentials(insecure.NewCredentials()))//connects to server. Insecure.newcredentials is used to skip TLS encryption for simplification
-		if err != nil {
-			log.Fatalf("conection start Not working")
-		}
-		
-		server.node_connections = append(server.node_connections, proto.NewNodeClient(conn))
-	
+	for i := 0; i < 3; i++ {
+	}
+	fmt.Print("loooping!")
+
+	conn, err := grpc.NewClient("localhost:5052", grpc.WithTransportCredentials(insecure.NewCredentials())) //connects to server. Insecure.newcredentials is used to skip TLS encryption for simplification
+	if err != nil {
+		log.Fatalf("conection start Not working")
+	}
+
+	server.node_connections = append(server.node_connections, proto.NewNodeClient(conn))
 
 	go loopOfLife()
-
-
 
 	server.start_server()
 }
@@ -108,6 +107,7 @@ func (s *Node) start_server() {
 	grpcServer := grpc.NewServer()              //Creates a new gRPC server instance
 	listener, err := net.Listen("tcp", ":5050") //Listens on TCP port 5050 using net.Listen
 	log.Println("Node server is up and running ")
+	fmt.Println("Node server is up and running ")
 	if err != nil {
 		log.Fatalf("failed to start server")
 	}
@@ -120,5 +120,3 @@ func (s *Node) start_server() {
 		log.Fatalf("Did not work")
 	}
 }
-
-
